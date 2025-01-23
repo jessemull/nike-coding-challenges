0a32684828cacb4743eb63fac476975e5dc6b387
Jesse Mull <Jesse.Mull@nike.com>
2025-01-23 08:27:18 -0800

    Updated questions for Jesus

--- src/main.js
+++ src/main.js
@@ -1,95 +1,114 @@
 function run() {
-    // Complete all problems below. Feel free to look up any information you'd like outside of the actual answers to
+    // Complete as many problems below as possible. Feel free to look up any information you'd like outside of the actual answers to
     // the questions. You can run the problems and see the output by executing this function.
 
-    // OUTPUT INTERSECTION OF TWO ARRAYS
+    // OUTPUT TRUE/FALSE
 
-    console.log("\n\n-----intersectionOfTwoArrays-----\n\n");
-    console.log(intersectionOfTwoArrays());
+    console.log("\n\n-----isAnagram-----\n\n");
+    console.log(isAnagram());
 
-    // OUTPUT LONGEST COMMON PREFIX
+    // OUTPUT ARRAY WITH ZEROES AT END
 
-    console.log("\n\n-----longestCommonPrefix-----\n\n");
-    console.log(longestCommonPrefix());
+    console.log("\n\n-----moveZeroes-----\n\n");
+    console.log(moveZeroes());
 
-    // OUTPUT PROVIDERS
+    // OUTPUT AVERAGE AGE
 
-    console.log("\n\n-----findChains-----\n\n");
-    console.log(findChains());
+    console.log("\n\n-----averageAge-----\n\n");
+    console.log(averageAge());
 
-    // OUTPUT TARGET INDEX OR -1
+    // OUTPUT ROMAN NUMERAL AS INTEGER
 
-    console.log("\n\n-----findTarget-----\n\n");
-    console.log(findTarget());
+    console.log("\n\n-----romanToInt-----\n\n");
+    console.log(romanToInt());
 }
 
 // Instructions:
-// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result 
-// must appear as many times as it shows in both arrays and you may return the result in any order.
-//
-// Example 1:
-// Input: nums1 = [1,2,2,1], nums2 = [2,2]
-// Output: [2,2]
-//
-// Example 2:
-// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-// Output: [4,9]
-// Explanation: [9,4] is also accepted.
-//
-function intersectionOfTwoArrays(nums1 = [4,9,5], nums2 = [9,4,9,8,4]) {
-
-}
-
-// Instructions:
-// Write a function to find the longest common prefix string amongst an array of strings. If there is no common 
-// prefix, return an empty string "".
+// Given two strings return true if one is an anagram of the other. An anagram is a word formed by rearranging the letters of another word.
 // 
-// Example 1:
-// Input: strs = ["flower","flow","flight"]
-// Output: "fl"
+// Example 1
+// Input: anagram, nagaram
+// Output: true
 //
-// Example 2:
-// Input: strs = ["dog","racecar","car"]
-// Output: ""
+// Example 2
+// Input: rat, car
+// Output: false
 //
-function longestCommonPrefix(s = ["flower","flow","flight"]) {
-
-}
-
-//  Instructions:
-//  The NPI registry has an open API to find healthcare provider organizations.
-//  https://npiregistry.cms.hhs.gov/registry/help-api
-//
-//  Given a zip code, get the first 10 providers for that zip code, and determine if any of those
-//  providers are have multiple locations by searching the NPI registry with the organization
-//  name. Return a comma delimited string of provider names that result in multiple locations.
-//
-//  Get up to 10 organizational providers by zip code:
-//  https://npiregistry.cms.hhs.gov/api/?version=2.1&postal_code=97005&enumeration_type=NPI-2
-//
-//  Get up to 10 providers by organization name:
-//  https://npiregistry.cms.hhs.gov/api/?version=2.1&enumeration_type=NPI-2&organization_name=THERAPEUTIC%20ASSOCIATES%20INC
-//
-function findChains(zipCode = "97005") {
+function isAnagram(s = "debit card", t = "bad credit") {
 
 }
 
 // Instructions:
-// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
-// You must write an algorithm with O(log n) runtime complexity.
+// Given an integer array of numbers, move all 0's to the end of it while maintaining the relative order of the non-zero elements in the array.
+// Do this in place without making a copy of the array.
 //
 // Example 1:
-// Input: nums = [-1,0,3,5,9,12], target = 9
-// Output: 4
-// Explanation: 9 exists in nums and its index is 4
+// Input: [0, 1, 0, 3, 12]
+// Output: [1, 3, 12, 0, 0]
 //
 // Example 2:
-// Input: nums = [-1,0,3,5,9,12], target = 2
-// Output: -1
-// Explanation: 2 does not exist in nums so return -1
+// Input: [0]
+// Output: [0]
 //
-function findTarget(nums = [-1,0,3,5,9,12], target = 9) {
-  
+function moveZeroes(nums) {
+   
+}
+
+// Instructions:
+// The website randomuser.me provides an API that produces randomly generated user information:
+// https://randomuser.me/
+//
+// Get 10 random user profiles of US nationality:
+// https://randomuser.me/api/?nat=us&results=10
+// 
+// The website api.agify.io provides an API that will predict someone's age based on their name:
+// https://agify.io/
+//
+// Predict someone's age based on their name:
+// https://api.agify.io?name=meelad
+//
+// Using the above APIs, fetch randomly generated data for a given number of users from the United States and return the predicted average age for their first names.
+//
+async function averageAge(numUsers = 10) {
+
+}
+
+// Instructions:
+// Given a roman numeral, convert it to an integer.
+//
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+//
+// Symbol       Value
+// I            1
+// V            5
+// X            10
+// L            50
+// C            100
+// D            500
+// M            1000
+//
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. 
+// Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same 
+// principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+//
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+//
+// Example 1:
+// Input: III
+// Output: 3
+//
+// Example 2:
+// Input: IV
+// Output: 4
+//
+// Example 3:
+// Input: IX
+// Output: 9
+//
+function romanToInt(s = 'IV') {
+    
 }
 
 run()
